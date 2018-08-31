53bf1c06a7aa3b57c15bccc362a7be0ca3a3ee4a
Jin Kwon <onacit@wemakeprice.com>
2018-08-31 13:57:58 +0900

    updated

--- src/main/java/com/github/jinahya/time/temporal/TemporalSlicer.java
+++ src/main/java/com/github/jinahya/time/temporal/TemporalSlicer.java
@@ -1,16 +1,12 @@
 package com.github.jinahya.time.temporal;
 
-import java.time.temporal.ChronoField;
 import java.time.temporal.Temporal;
-import java.util.ArrayList;
+import java.time.temporal.TemporalAdjuster;
 import java.util.Comparator;
-import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
-import java.util.function.UnaryOperator;
 import java.util.logging.Logger;
 
-import static java.lang.String.format;
 import static java.lang.invoke.MethodHandles.lookup;
 import static java.util.logging.Logger.getLogger;
 
@@ -29,140 +25,177 @@ public class TemporalSlicer {
     // -----------------------------------------------------------------------------------------------------------------
 
 
-    public static <T extends Temporal> void slice(
-            final T start, final T end,
-            final BiPredicate<T, T> comparator,
-            final UnaryOperator<T> discriminator,
-            final UnaryOperator<T> incrementer,
-            final BiConsumer<T, T> consumer) {
-        T s, e;
-        s = discriminator.apply(start);
-        System.out.println("first s: " + s);
-        e = incrementer.apply(s);
-        if (comparator.test(s, start)) {
-            s = start;
-            while (comparator.test(e, s)) {
-                e = incrementer.apply(e);
-            }
-        }
-        while (true) {
-            if (comparator.test(end, e)) {
-                consumer.accept(s, end);
-                break;
-            }
-            consumer.accept(s, e);
-            s = e;
-            e = incrementer.apply(e);
-        }
-    }
+//    public static <T extends Temporal> void slice(
+//            final T start, final T end,
+//            final BiPredicate<T, T> comparator,
+//            final UnaryOperator<T> discriminator,
+//            final UnaryOperator<T> incrementer,
+//            final BiConsumer<T, T> consumer) {
+//        T s, e;
+//        s = discriminator.apply(start);
+//        System.out.println("first s: " + s);
+//        e = incrementer.apply(s);
+//        if (comparator.test(s, start)) {
+//            s = start;
+//            while (comparator.test(e, s)) {
+//                e = incrementer.apply(e);
+//            }
+//        }
+//        while (true) {
+//            if (comparator.test(end, e)) {
+//                consumer.accept(s, end);
+//                break;
+//            }
+//            consumer.accept(s, e);
+//            s = e;
+//            e = incrementer.apply(e);
+//        }
+//    }
+//
+//    public static <T extends Temporal & Comparable<? super T>> List<TemporalSlice<T>> slice(
+//            final T start, final T end, final UnaryOperator<T> discriminator, final UnaryOperator<T> incrementer) {
+//        logger.info(() -> format(">>>>>: %1$s %2$s %3$d %4$d", start, end, start.get(ChronoField.NANO_OF_SECOND), end.get(ChronoField.NANO_OF_SECOND)));
+//        if (start == null) {
+//            throw new NullPointerException("start is null");
+//        }
+//        if (end == null) {
+//            throw new NullPointerException("end is null");
+//        }
+//        if (start.compareTo(end) > 0) {
+//            throw new IllegalArgumentException("start...");
+//        }
+//        T s = discriminator.apply(start);
+//        if (s.compareTo(start) > 0) {
+//            throw new RuntimeException("discriminator yielded a future temporal(" + s + ") for start(" + start + ")");
+//        }
+//        T e = incrementer.apply(s);
+//        if (e.compareTo(s) < 0) {
+//            throw new RuntimeException("incrementer yielded a past temporal(" + e + ") for " + s);
+//        }
+//        if (s.compareTo(start) < 0) {
+//            while (e.compareTo(s) < 0) {
+//                e = incrementer.apply(e);
+//            }
+//            s = start;
+//        }
+//        final List<TemporalSlice<T>> list = new ArrayList<>();
+//        while (true) {
+//            if (end.compareTo(e) < 0) {
+//                list.add(TemporalSlice.of(s, end));
+//                break;
+//            }
+//            list.add(TemporalSlice.of(s, e));
+//            s = e;
+//            e = incrementer.apply(e);
+//        }
+//        return list;
+//    }
+//
+//    public static <T extends Temporal & Comparable<? super T>> void slice1(
+//            final T start, final T end, final UnaryOperator<T> discriminator, final UnaryOperator<T> incrementer) {
+//        logger.info(() -> format(">>>>>: %1$s %2$s %3$d %4$d", start, end, start.get(ChronoField.NANO_OF_SECOND), end.get(ChronoField.NANO_OF_SECOND)));
+//        if (start == null) {
+//            throw new NullPointerException("start is null");
+//        }
+//        if (end == null) {
+//            throw new NullPointerException("end is null");
+//        }
+//        if (start.compareTo(end) > 0) {
+//            throw new IllegalArgumentException("start...");
+//        }
+//        T s = discriminator.apply(start);
+//        if (s.compareTo(start) > 0) {
+//            throw new RuntimeException("discriminator yielded a future temporal(" + s + ") for start(" + start + ")");
+//        }
+//        T e = incrementer.apply(s);
+//        if (e.compareTo(s) < 0) {
+//            throw new RuntimeException("incrementer yielded a past temporal(" + e + ") for " + s);
+//        }
+//        if (s.compareTo(start) < 0) {
+//            while (e.compareTo(s) < 0) {
+//                e = incrementer.apply(e);
+//            }
+//            s = start;
+//        }
+//        final List<TemporalSlice<T>> list = new ArrayList<>();
+//        while (true) {
+//            if (end.compareTo(e) < 0) {
+//                list.add(TemporalSlice.of(s, end));
+//                break;
+//            }
+//            list.add(TemporalSlice.of(s, e));
+//            s = e;
+//            e = incrementer.apply(e);
+//        }
+//    }
+//
+//    public static <T extends Temporal> void slice2(
+//            final T start, final T end,
+////            final BiPredicate<? super T, ? super T> temporalComparator,
+//            final Comparator<? super T> temporalComparator,
+//            final UnaryOperator<T> startIdentifier, final UnaryOperator<T> endIncrementer) {
+//        if (start == null) {
+//            throw new NullPointerException("start is null");
+//        }
+//        if (end == null) {
+//            throw new NullPointerException("end is null");
+//        }
+//        T s = startIdentifier.apply(start);
+//        T e = endIncrementer.apply(s);
+//        if (temporalComparator.compare(s, start) < 0) {
+//            while (temporalComparator.compare(e, s) < 0) {
+//                e = endIncrementer.apply(e);
+//            }
+//            s = start;
+//        }
+//        while (true) {
+//            if (temporalComparator.compare(end, e) <= 0) {
+//                final T s1 = s;
+//                System.out.printf("%1$S ~ %2$s\n", s1, end);
+//                break;
+//            }
+//            final T s1 = s;
+//            final T e1 = e;
+//            System.out.printf("%1$S ~ %2$s\n", s1, e1);
+//            s = e;
+//            e = endIncrementer.apply(e);
+//        }
+//    }
 
-    public static <T extends Temporal & Comparable<? super T>> List<TemporalSlice<T>> slice(
-            final T start, final T end, final UnaryOperator<T> discriminator, final UnaryOperator<T> incrementer) {
-        logger.info(() -> format(">>>>>: %1$s %2$s %3$d %4$d", start, end, start.get(ChronoField.NANO_OF_SECOND), end.get(ChronoField.NANO_OF_SECOND)));
+    public static void slice3(
+            final Temporal start, final Temporal end,
+            final Comparator<? super Temporal> temporalComparator,
+            final TemporalAdjuster startAdjuster, final TemporalAdjuster endAdjuster,
+            final BiConsumer<Temporal, Temporal> consumer) {
         if (start == null) {
             throw new NullPointerException("start is null");
         }
         if (end == null) {
             throw new NullPointerException("end is null");
         }
-        if (start.compareTo(end) > 0) {
-            throw new IllegalArgumentException("start...");
-        }
-        T s = discriminator.apply(start);
-        if (s.compareTo(start) > 0) {
-            throw new RuntimeException("discriminator yielded a future temporal(" + s + ") for start(" + start + ")");
-        }
-        T e = incrementer.apply(s);
-        if (e.compareTo(s) < 0) {
-            throw new RuntimeException("incrementer yielded a past temporal(" + e + ") for " + s);
-        }
-        if (s.compareTo(start) < 0) {
-            while (e.compareTo(s) < 0) {
-                e = incrementer.apply(e);
-            }
-            s = start;
-        }
-        final List<TemporalSlice<T>> list = new ArrayList<>();
-        while (true) {
-            if (end.compareTo(e) < 0) {
-                list.add(TemporalSlice.of(s, end));
-                break;
-            }
-            list.add(TemporalSlice.of(s, e));
-            s = e;
-            e = incrementer.apply(e);
-        }
-        return list;
-    }
-
-    public static <T extends Temporal & Comparable<? super T>> void slice1(
-            final T start, final T end, final UnaryOperator<T> discriminator, final UnaryOperator<T> incrementer) {
-        logger.info(() -> format(">>>>>: %1$s %2$s %3$d %4$d", start, end, start.get(ChronoField.NANO_OF_SECOND), end.get(ChronoField.NANO_OF_SECOND)));
-        if (start == null) {
-            throw new NullPointerException("start is null");
-        }
-        if (end == null) {
-            throw new NullPointerException("end is null");
-        }
-        if (start.compareTo(end) > 0) {
-            throw new IllegalArgumentException("start...");
-        }
-        T s = discriminator.apply(start);
-        if (s.compareTo(start) > 0) {
-            throw new RuntimeException("discriminator yielded a future temporal(" + s + ") for start(" + start + ")");
-        }
-        T e = incrementer.apply(s);
-        if (e.compareTo(s) < 0) {
-            throw new RuntimeException("incrementer yielded a past temporal(" + e + ") for " + s);
-        }
-        if (s.compareTo(start) < 0) {
-            while (e.compareTo(s) < 0) {
-                e = incrementer.apply(e);
-            }
-            s = start;
-        }
-        final List<TemporalSlice<T>> list = new ArrayList<>();
-        while (true) {
-            if (end.compareTo(e) < 0) {
-                list.add(TemporalSlice.of(s, end));
-                break;
-            }
-            list.add(TemporalSlice.of(s, e));
-            s = e;
-            e = incrementer.apply(e);
-        }
-    }
-
-    public static <T extends Temporal> void slice2(
-            final T start, final T end,
-//            final BiPredicate<? super T, ? super T> temporalComparator,
-            final Comparator<? super T> temporalComparator,
-            final UnaryOperator<T> startIdentifier, final UnaryOperator<T> endIncrementer) {
-        if (start == null) {
-            throw new NullPointerException("start is null");
-        }
-        if (end == null) {
-            throw new NullPointerException("end is null");
-        }
-        T s = startIdentifier.apply(start);
-        T e = endIncrementer.apply(s);
+        Temporal s = start.with(startAdjuster);
+        Temporal e = s.with(endAdjuster);
         if (temporalComparator.compare(s, start) < 0) {
-            while (temporalComparator.compare(e, s) < 0) {
-                e = endIncrementer.apply(e);
-            }
             s = start;
+        }
+        while (temporalComparator.compare(e, s) < 0) {
+            e = e.with(endAdjuster);
         }
         while (true) {
             if (temporalComparator.compare(end, e) <= 0) {
-                final T s1 = s;
+                final Temporal s1 = s;
                 System.out.printf("%1$S ~ %2$s\n", s1, end);
                 break;
             }
-            final T s1 = s;
-            final T e1 = e;
+            final Temporal s1 = s;
+            final Temporal e1 = e;
             System.out.printf("%1$S ~ %2$s\n", s1, e1);
             s = e;
-            e = endIncrementer.apply(e);
+            e = e.with(endAdjuster);
+            if (temporalComparator.compare(s, e) > 0) {
+                throw new RuntimeException("adjusted end(" + e + ") < previous end(" + s + ")");
+            }
         }
     }
+
 }
--- src/test/java/com/github/jinahya/time/temporal/TemporalSlicerTest.java
+++ src/test/java/com/github/jinahya/time/temporal/TemporalSlicerTest.java
@@ -8,7 +8,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
+import java.util.Comparator;
 
 import static java.lang.invoke.MethodHandles.lookup;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -19,58 +21,84 @@ public class TemporalSlicerTest {
     private static final Logger logger = getLogger(lookup().lookupClass());
 
     // -----------------------------------------------------------------------------------------------------------------
-    @Test
-    @Disabled
-    public void testLocalDateForYearByMonth() {
-        final LocalDate start = LocalDate.now();
-        final LocalDate end = start.plus(1L, ChronoUnit.YEARS);
-        System.out.printf("%1$s , %2$s\n", start, end);
-        TemporalSlicer.slice(
-                start,
-                end,
-                (t1, t2) -> {
-                    //System.out.printf("comparing %1$s <> %2$s\n", t1, t2);
-                    boolean r = t1.equals(t2) || t1.isBefore(t2);
-                    //System.out.println(r);
-                    return r;
-                },
-                t -> t.with(TemporalAdjusters.firstDayOfYear()),
-                t -> t.plus(1, ChronoUnit.MONTHS),
-                (s, e) -> System.out.printf("%1$s ~ %2$s\n", s, e)
-                            );
-    }
-
-    @Test
-    @Disabled
-    public void testLocalDateTimeForYearByMonth() {
-        //final LocalDateTime start = LocalDate.now().atStartOfDay();
-        final LocalDateTime start = LocalDateTime.now();
-        final LocalDateTime end = start.plus(1L, ChronoUnit.YEARS);
-        System.out.printf("%1$s , %2$s\n", start, end);
-        TemporalSlicer.slice(
-                start,
-                end,
-                (t1, t2) -> {
-                    //System.out.printf("comparing %1$s <> %2$s\n", t1, t2);
-                    boolean r = t1.equals(t2) || t1.isBefore(t2);
-                    //System.out.println(r);
-                    return r;
-                },
-                t -> LocalDate.now().atStartOfDay(),
-                t -> t.plus(1, ChronoUnit.MONTHS),
-                (s, e) -> System.out.printf("%1$s ~ %2$s\n", s, e)
-                            );
-    }
-
 //    @Test
 //    @Disabled
-//    public void testLocalDateTimeFromNowForYearByMonth() {
-//        final LocalDateTime start = LocalDateTime.now();
-//        final LocalDateTime end = start.plus(1L, ChronoUnit.YEARS);
-//        logger.debug("duration: {}", Duration.between(start, end));
-//        final List<TemporalSlice<LocalDateTime>> slices = TemporalSlicer.slice(
+//    public void testLocalDateForYearByMonth() {
+//        final LocalDate start = LocalDate.now();
+//        final LocalDate end = start.plus(1L, ChronoUnit.YEARS);
+//        System.out.printf("%1$s , %2$s\n", start, end);
+//        TemporalSlicer.slice(
 //                start,
 //                end,
+//                (t1, t2) -> {
+//                    //System.out.printf("comparing %1$s <> %2$s\n", t1, t2);
+//                    boolean r = t1.equals(t2) || t1.isBefore(t2);
+//                    //System.out.println(r);
+//                    return r;
+//                },
+//                t -> t.with(TemporalAdjusters.firstDayOfYear()),
+//                t -> t.plus(1, ChronoUnit.MONTHS),
+//                (s, e) -> System.out.printf("%1$s ~ %2$s\n", s, e)
+//                            );
+//    }
+//
+//    @Test
+//    @Disabled
+//    public void testLocalDateTimeForYearByMonth() {
+//        //final LocalDateTime start = LocalDate.now().atStartOfDay();
+//        final LocalDateTime start = LocalDateTime.now();
+//        final LocalDateTime end = start.plus(1L, ChronoUnit.YEARS);
+//        System.out.printf("%1$s , %2$s\n", start, end);
+//        TemporalSlicer.slice(
+//                start,
+//                end,
+//                (t1, t2) -> {
+//                    //System.out.printf("comparing %1$s <> %2$s\n", t1, t2);
+//                    boolean r = t1.equals(t2) || t1.isBefore(t2);
+//                    //System.out.println(r);
+//                    return r;
+//                },
+//                t -> LocalDate.now().atStartOfDay(),
+//                t -> t.plus(1, ChronoUnit.MONTHS),
+//                (s, e) -> System.out.printf("%1$s ~ %2$s\n", s, e)
+//                            );
+//    }
+//
+////    @Test
+////    @Disabled
+////    public void testLocalDateTimeFromNowForYearByMonth() {
+////        final LocalDateTime start = LocalDateTime.now();
+////        final LocalDateTime end = start.plus(1L, ChronoUnit.YEARS);
+////        logger.debug("duration: {}", Duration.between(start, end));
+////        final List<TemporalSlice<LocalDateTime>> slices = TemporalSlicer.slice(
+////                start,
+////                end,
+////                t -> t.with(ChronoField.DAY_OF_MONTH, 1L)
+////                        .with(ChronoField.HOUR_OF_DAY, 0L)
+////                        .with(ChronoField.MINUTE_OF_HOUR, 0L)
+////                        .with(ChronoField.SECOND_OF_MINUTE, 0L)
+////                        .with(ChronoField.MILLI_OF_SECOND, 0L)
+////                        .with(ChronoField.MICRO_OF_SECOND, 0L)
+////                        .with(ChronoField.NANO_OF_SECOND, 0L)
+////                ,
+////                t -> t.plus(1L, ChronoUnit.MONTHS),
+////                UnaryOperator.identity());
+////        for (final TemporalSlice<LocalDateTime> slice : slices) {
+////            logger.debug("slice: {} / {} / {}", slice, slice.getStartInclusive().get(ChronoField.NANO_OF_SECOND), slice.getStartInclusive().get(ChronoField.NANO_OF_SECOND));
+////        }
+////    }
+//
+//    @Test
+//    @Disabled
+//    public void testLocalDateTimeFromNowForYearByMonthIncrement() {
+//        logger.debug("inc...");
+//        final LocalDateTime start = LocalDateTime.now();
+//        final LocalDateTime end = start.plus(1L, ChronoUnit.YEARS);
+//        TemporalSlicer.slice2(
+//                start,
+//                end,
+//                (t1, t2) -> t1.compareTo(t2),
+////                TemporalSlicer.<LocalDateTime>natural(),
 //                t -> t.with(ChronoField.DAY_OF_MONTH, 1L)
 //                        .with(ChronoField.HOUR_OF_DAY, 0L)
 //                        .with(ChronoField.MINUTE_OF_HOUR, 0L)
@@ -79,23 +107,64 @@ public class TemporalSlicerTest {
 //                        .with(ChronoField.MICRO_OF_SECOND, 0L)
 //                        .with(ChronoField.NANO_OF_SECOND, 0L)
 //                ,
-//                t -> t.plus(1L, ChronoUnit.MONTHS),
-//                UnaryOperator.identity());
-//        for (final TemporalSlice<LocalDateTime> slice : slices) {
-//            logger.debug("slice: {} / {} / {}", slice, slice.getStartInclusive().get(ChronoField.NANO_OF_SECOND), slice.getStartInclusive().get(ChronoField.NANO_OF_SECOND));
-//        }
+//                t -> t.plus(1L, ChronoUnit.MONTHS));
+//    }
+//
+//    @Test
+//    @Disabled
+//    public void testLocalDateTimeFromNowForYearByMonthDecrement() {
+//        logger.debug("dec...");
+//        final LocalDateTime start = LocalDateTime.now();
+//        final LocalDateTime end = start.minus(1L, ChronoUnit.YEARS);
+//        TemporalSlicer.slice2(
+//                start,
+//                end,
+//                (t1, t2) -> t2.compareTo(t1),
+////                TemporalSlicer.<LocalDateTime>natural().negate(),
+//                t -> t.with(ChronoField.DAY_OF_MONTH, 1L)
+//                        .with(ChronoField.HOUR_OF_DAY, 0L)
+//                        .with(ChronoField.MINUTE_OF_HOUR, 0L)
+//                        .with(ChronoField.SECOND_OF_MINUTE, 0L)
+//                        .with(ChronoField.MILLI_OF_SECOND, 0L)
+//                        .with(ChronoField.MICRO_OF_SECOND, 0L)
+//                        .with(ChronoField.NANO_OF_SECOND, 0L)
+//                        .plus(1L, ChronoUnit.MONTHS)
+//                ,
+//                t -> t.minus(1L, ChronoUnit.MONTHS));
+//    }
+//
+//    @Test
+//    @Disabled
+//    public void testLocalDateTimeFromNowToNow() {
+//        final LocalDateTime start = LocalDateTime.now();
+//        final LocalDateTime end = start;
+//        TemporalSlicer.slice2(
+//                start,
+//                end,
+//                (t1, t2) -> t2.compareTo(t1),
+////                TemporalSlicer.<LocalDateTime>natural().negate(),
+//                t -> t.with(ChronoField.DAY_OF_MONTH, 1L)
+//                        .with(ChronoField.HOUR_OF_DAY, 0L)
+//                        .with(ChronoField.MINUTE_OF_HOUR, 0L)
+//                        .with(ChronoField.SECOND_OF_MINUTE, 0L)
+//                        .with(ChronoField.MILLI_OF_SECOND, 0L)
+//                        .with(ChronoField.MICRO_OF_SECOND, 0L)
+//                        .with(ChronoField.NANO_OF_SECOND, 0L)
+//                        .plus(1L, ChronoUnit.MONTHS)
+//                ,
+//                t -> t.minus(1L, ChronoUnit.MONTHS));
 //    }
 
+    // -----------------------------------------------------------------------------------------------------------------
     @Test
-    public void testLocalDateTimeFromNowForYearByMonthIncrement() {
-        logger.debug("inc...");
+    public void testSlice3LocalDateTimeFromNowForYearByMonthIncrement() {
+        logger.debug("not to a year after");
         final LocalDateTime start = LocalDateTime.now();
         final LocalDateTime end = start.plus(1L, ChronoUnit.YEARS);
-        TemporalSlicer.slice2(
+        TemporalSlicer.slice3(
                 start,
                 end,
-                (t1, t2) -> t1.compareTo(t2),
-//                TemporalSlicer.<LocalDateTime>natural(),
+                Comparator.comparing(t2 -> ((LocalDateTime) t2)),
                 t -> t.with(ChronoField.DAY_OF_MONTH, 1L)
                         .with(ChronoField.HOUR_OF_DAY, 0L)
                         .with(ChronoField.MINUTE_OF_HOUR, 0L)
@@ -108,14 +177,16 @@ public class TemporalSlicerTest {
     }
 
     @Test
-    public void testLocalDateTimeFromNowForYearByMonthDecrement() {
-        logger.debug("dec...");
+    //@Disabled
+    public void testSlice3LocalDateTimeFromNowForYearByMonthDecrement() {
+        logger.debug("now to a year ago");
         final LocalDateTime start = LocalDateTime.now();
         final LocalDateTime end = start.minus(1L, ChronoUnit.YEARS);
-        TemporalSlicer.slice2(
+        TemporalSlicer.slice3(
                 start,
                 end,
-                (t1, t2) -> t2.compareTo(t1),
+                Comparator.comparing(t2 -> ((LocalDateTime) t2)).reversed(),
+//                (t1, t2) -> ((LocalDateTime) t2).compareTo((LocalDateTime) t1),
 //                TemporalSlicer.<LocalDateTime>natural().negate(),
                 t -> t.with(ChronoField.DAY_OF_MONTH, 1L)
                         .with(ChronoField.HOUR_OF_DAY, 0L)
@@ -130,23 +201,17 @@ public class TemporalSlicerTest {
     }
 
     @Test
-    public void testLocalDateTimeFromNowToNow() {
+    //@Disabled
+    public void testSlice3LocalDateTimeFromNowToNow() {
+        logger.debug("now-to-now");
         final LocalDateTime start = LocalDateTime.now();
         final LocalDateTime end = start;
-        TemporalSlicer.slice2(
+        TemporalSlicer.slice3(
                 start,
                 end,
-                (t1, t2) -> t2.compareTo(t1),
+                Comparator.comparing(t2 -> ((LocalDateTime) t2)).reversed(),
 //                TemporalSlicer.<LocalDateTime>natural().negate(),
-                t -> t.with(ChronoField.DAY_OF_MONTH, 1L)
-                        .with(ChronoField.HOUR_OF_DAY, 0L)
-                        .with(ChronoField.MINUTE_OF_HOUR, 0L)
-                        .with(ChronoField.SECOND_OF_MINUTE, 0L)
-                        .with(ChronoField.MILLI_OF_SECOND, 0L)
-                        .with(ChronoField.MICRO_OF_SECOND, 0L)
-                        .with(ChronoField.NANO_OF_SECOND, 0L)
-                        .plus(1L, ChronoUnit.MONTHS)
-                ,
-                t -> t.minus(1L, ChronoUnit.MONTHS));
+                t -> t,
+                t -> t);
     }
 }
